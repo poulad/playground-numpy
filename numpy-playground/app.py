75086888963a3b65474664f7213d1d310a796d47
Poulad <poulad.pld@gmail.com>
2019-08-22 22:23:17 -0400

    Reshape arrays

--- numpy-playground/app.py
+++ numpy-playground/app.py
@@ -1,3 +1,19 @@
 import numpy as np
 
-np.arrange(15)
+a: np.ndarray = np.arange(15).reshape(3, 5)
+print(f"Data type is {a.dtype}")
+
+b = np.empty((2, 3), np.int32)
+print(b)
+
+a_linspace = np.linspace(0, 2 * np.pi, 20)
+print(np.sin(a_linspace))
+
+a_random: np.ndarray = np.floor(10 * np.random.random((3, 3)))
+
+print(f"a_random array is: \n{a_random}")
+print(f"falttened: {a_random.ravel()}")
+print(f"transposed: \n {a_random.T}")
+
+a_random.resize((4, 5))
+print(f"a_random array resized:\n{a_random}")
