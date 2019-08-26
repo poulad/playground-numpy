d430f9ebe34afae27170898dfa9c1310356e5810
Poulad <poulad.pld@gmail.com>
2019-08-26 09:05:41 -0400

    Stacking arrays

---
+++ numpy-playground/stacking.py
@@ -0,0 +1,19 @@
+import numpy as np
+from numpy import newaxis
+
+a = np.array(np.floor(np.random.random((2, 2)) * 56), dtype=np.int16)
+b = np.array(np.ceil(np.random.random((2, 2)) * 49), dtype=np.int16)
+print(f"Array a\n:{a}")
+print(f"Array b\n:{b}")
+
+print(
+    "Horizontal stack:", np.hstack((a, b)),
+    "Vertical stack:", np.vstack((a, b)),
+    sep="\n"
+)
+
+c = np.arange(1, stop=6, step=2)
+print(f"Array C:\n{c}")
+# add a new axis to it
+print(f"Reshaping C into a 3x1 matrix:\n{c[:, np.newaxis]}")
+print(f"Reshaping C into a 1x3 matrix:\n{c[newaxis, :]}")
