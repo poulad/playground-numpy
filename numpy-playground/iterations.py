a099132b3644ddfc3bee6de61dca8c365bd07f79
Poulad <poulad.pld@gmail.com>
2019-08-26 01:03:20 -0400

    More array iterations

--- numpy-playground/iterations.py
+++ numpy-playground/iterations.py
@@ -5,7 +5,14 @@ a_random = np.array(np.floor(np.random.random((2, 4)) * 35))
 print(f"Randomly generated array:\n{a_random}")
 print(f"Reversed:\n{a_random[::-1]}")
 
-a_func = np.fromfunction(lambda x, y: 2 * x + y, (3, 5))
+a_func = np.fromfunction(lambda x, y: 2 * x + y, (4, 6), dtype=np.int16)
 print(f"2x+y:\n{a_func}")
 
-print(f"its 3rs column is: {a_func[:, 2]}")
+print(f"its 3rd column is: {a_func[:, 2]}")
+print(f"2nd and 4th rows are:\n{a_func[1:4:2, :]}")
+
+print("array elements are: [ ", end="")
+for el in a_func.flat:
+    print(f"{el}, ", end='')
+else:
+    print("]")
