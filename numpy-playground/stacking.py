b5b9274acf5cfad2c1ba89f362d6ab2531d068f4
Poulad <poulad.pld@gmail.com>
2019-08-26 20:19:16 -0400

    Split arrays

--- numpy-playground/stacking.py
+++ numpy-playground/stacking.py
@@ -17,3 +17,8 @@ print(f"Array C:\n{c}")
 # add a new axis to it
 print(f"Reshaping C into a 3x1 matrix:\n{c[:, np.newaxis]}")
 print(f"Reshaping C into a 1x3 matrix:\n{c[newaxis, :]}")
+
+d = np.array(np.floor(np.random.random((4, 5)) * 57), dtype=np.int16)
+print(f"Array d:\n{d}")
+print(f"split into 3 arrays horizontally between 2nd and 3rd columns:\n{np.hsplit(d, (1, 2))}")
+
